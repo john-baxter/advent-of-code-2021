778e06e89acfeb2dd00173af586665c38a685c92
john-baxter <john.g.baxter@googlemail.com>
2021-12-01 13:30:37 +0000

    Add solution for 1-2.

--- 20211201_2_sonar_sweep.py
+++ 20211201_2_sonar_sweep.py
@@ -2,12 +2,12 @@ input_text = open('20211201_sonar_sweep_input.txt')
 input_list = list(input_text)
 input_list = [int(strint) for strint in input_list]
 
-def how_many_increases_refined(list_of_depths):
+def how_many_increases_wide(list_of_depths):
   total_increases = 0
-  for i in range(1, len(list_of_depths)):
-    if list_of_depths[i] > list_of_depths[i-1]:
+  for i in range(3, len(list_of_depths)):
+    if list_of_depths[i] > list_of_depths[i-3]:
       total_increases += 1
       
   return total_increases
 
-print(how_many_increases_refined(input_list))
+print(how_many_increases_wide(input_list))
