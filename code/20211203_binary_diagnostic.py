947bd57be8bdfa6b89d09b362b0adf88602e3888
john-baxter <john.g.baxter@googlemail.com>
2021-12-03 12:34:57 +0000

    Add documentation to all functions in day 3 .py file.

--- code/20211203_binary_diagnostic.py
+++ code/20211203_binary_diagnostic.py
@@ -1,9 +1,38 @@
+"""
+Setup
+-----
+The input data has been copied into a .txt file
+Use code to convert this .txt into a list of (string) binary numbers, removing 
+the trailing newline from each.
+"""
 input_text = open('inputs/20211203_binary_diagnostic.txt')
 input_list = list(input_text)
 input_list = [binary_number.rstrip("\n") for binary_number in input_list]
 
 
 def total_each_column(list_of_string_numbers):
+  """Converts a list of numbers (as strings) and converts it into a new list containing 
+  the total of each 'column' of numbers.
+  
+  Will be used to calculate how many '1's are in each corresponding position in the 
+  diagnostic report.
+  
+  Assumption
+  ----------
+  Every number in the list has the same amount of digits.
+  
+  Parameters
+  ----------
+  (list) : list_of_string_numbers
+    A list of arbitrary length contining numbers as strings
+    
+  Returns
+  -------
+  (list) : list_of_column_totals
+    A list with length equivalent to the number of digits in each number. Contains the 
+    total values of the sum of all digits in each corresponding position.
+    Elements are integers.
+  """
   # assumes all elements in list_of_string_numbers have same len(element)
   list_of_column_totals = []
   for i in range(len(list_of_string_numbers[0])):
@@ -15,6 +44,22 @@ def total_each_column(list_of_string_numbers):
 
 
 def interrogate_column_totals_gamma(list_of_ints):
+  """Converts each column total to either 1 or 0 as appropriate for the 'gamma rate'
+  
+  References len(input_list) (from above) and if the column total is more than half, 
+  that corresponding position is populated by (int) 1; if less than half, populated 
+  by (int) 0
+  
+  Parameters
+  ----------
+  (list) : list_of_ints
+    A list of integers, values are arbitrary.
+    
+  Returns
+  -------
+  (list) : list_of_bits
+    A list of integers, values are either 0 or 1
+  """
   list_of_bits = []
   for i in list_of_ints:
     if i > len(input_list) // 2:
@@ -25,14 +70,52 @@ def interrogate_column_totals_gamma(list_of_ints):
 
 
 def convert_list_of_bits_to_decimal_integer(list_of_bits):
+  """Converts a list of bits into the corresponding decimal value
+  
+  Parameters
+  ----------
+  (list) : list_of_bits
+    A list of integers, the values are 0 or 1
+    
+  Returns
+  -------
+  (int) : The integer value corresponding to the result of joining the given bits into 
+  a binary integer and converting that to decimal.
+  """
   return int("".join(str(bit) for bit in list_of_bits), 2)
 
 
 def derive_epsilon_from_gamma(list_of_bits):
+  """Converts 0 to 1, and 1 to 0 for a given list of bits
+  
+  Parameters
+  ----------
+  (list) : list_of_bits
+    A list of integers, the values are 0 or 1
+    
+  Returns
+  -------
+  (list)
+    A list of bits of same length as given list_of_bits, each bit has been converted 
+    to its 'opposite' value
+  
+  """
   return [int(not bit) for bit in list_of_bits]
 
 
 def get_gamma_rate(input_list):
+  """Calls all relevant functions to derive the 'gamma rate' from the input list
+  
+  Parameters
+  ----------
+  (list) : input_list
+    As above, the input text given by the challenge converted to binary numbers as strings
+    
+  Returns
+  -------
+  (int)
+    The 'gamma rate' following the logic as per the challenge information
+  """
   return convert_list_of_bits_to_decimal_integer(
     interrogate_column_totals_gamma(
       total_each_column(input_list)
@@ -41,6 +124,18 @@ def get_gamma_rate(input_list):
 
 
 def get_epsilon_rate(input_list):
+  """Calls all relevant functions to derive the 'epsilon rate' from the input list
+  
+  Parameters
+  ----------
+  (list) : input_list
+    As above, the input text given by the challenge converted to binary numbers as strings
+    
+  Returns
+  -------
+  (int)
+    The 'epsilon rate' following the logic as per the challenge information
+  """
   return convert_list_of_bits_to_decimal_integer(
     derive_epsilon_from_gamma(
       interrogate_column_totals_gamma(
@@ -51,6 +146,20 @@ def get_epsilon_rate(input_list):
 
 
 def calculate_power_consumption(input_list):
+  """Calculates the power consumption following the logic as per the challenge 
+  information by calling relevant functions, then multiplying the gamma 
+  and epsilon rates
+  
+  Parameters
+  ----------
+  (list) : input_list
+    As above, the input text given by the challenge converted to binary numbers as strings
+    
+  Returns
+  -------
+  (int)
+  The 'power consumption' following the logic as per the challenge information
+  """
   return get_gamma_rate(input_list) * get_epsilon_rate(input_list)
 
 """
