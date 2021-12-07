426ba7bc4a0a84f67f00f5d216bd0fa8cb4f4e71
john-baxter <john.g.baxter@googlemail.com>
2021-12-07 08:00:29 +0000

    Add solution to Day 6.

--- code/20211206_lanternfish.py
+++ code/20211206_lanternfish.py
@@ -5,7 +5,8 @@ input_list = [int(i) for i in input_string.split(',')]
 
 new_parent_status = 6
 baby_lanternfish_status = 8
-days_to_simulate = 80
+days_to_simulate_part_one = 80
+days_to_simulate_part_two = 256
 
 
 def propagate_lanternfish(lanternfish_list):
@@ -32,8 +33,28 @@ def process_one_day_of_lanternfish_reproduction(lanternfish_list):
 
 def count_lanternfish_after_x_days(lanternfish_list, days):
   for day in range(days):
+    print(day)
     lanternfish_list = process_one_day_of_lanternfish_reproduction(lanternfish_list)
   return len(lanternfish_list)
 
 
-print(count_lanternfish_after_x_days(input_list, days_to_simulate))
+lanternfish_life_stage_tally_list = [0 for i in range(9)]
+for fish in input_list:
+  lanternfish_life_stage_tally_list[fish] += 1
+  
+
+def update_tally_list_for_one_day(tally_list):
+  placeholder_for_idx_0 = tally_list[0]
+  tally_list = tally_list[1:]
+  tally_list.append(placeholder_for_idx_0)
+  tally_list[6] += placeholder_for_idx_0
+  return tally_list
+
+
+def simulate_life_cycle_propagation_of_lanternfish(tally_list, days_in_simulation):
+  for i in range(days_in_simulation):
+    tally_list = update_tally_list_for_one_day(tally_list)
+  return sum(tally_list)
+
+
+print(simulate_life_cycle_propagation_of_lanternfish(lanternfish_life_stage_tally_list, 256))
